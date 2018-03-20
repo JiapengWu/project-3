b60110a5deb5a2fe087bdfd16be8920b64367566
mathieuvachon2 <vmat96@gmail.com>
2018-03-20 12:41:13 -0400

    menu for now

--- main.py
+++ main.py
@@ -6,7 +6,51 @@ config = "user='cs421g19' host='comp421.cs.mcgill.ca' dbname='cs421' password='P
 
 connection = psycopg2.connect(config)
 cur = connection.cursor()
-m.doQuery(cur)
+
+option = input("select 1 to execute query 1\nselect 2 to execute query 2\nselect 3 to execute query 3\nselect 4 to execute query 4\nselect 5 to execute query 5")
+
+# Redirects to option that user chose
+if(option == 1) option_1
+elif (option == 2) option_2
+elif (option == 3) option_3
+elif (option == 4) option_4
+elif (option == 5) option_5
+else print 'You did not choose a correct option.'
+
+# The add single player option
+def option_1() :
+    # Input player ID
+    pid = input("What is the player's pid? It has to be greater than 0")
+    if (pid < 0) raise PIDError('Pid less than 0')
+
+    # Input name
+    try:
+        name = raw_input("What is the player's name?")
+    except ValueError:
+        raise NameError("Invalid name")
+    # Gender
+    gender = raw_input("What is the player's gender") 
+    if (gender != "Male" and gender != Female) raise GenderError('Unvalid gender')
+
+    # What is the nationality
+    try:
+        nationality = raw_input("What is the player's country")
+    except ValueError:
+        raise NatError("Invalid nationality")
+
+    m.add_single_player(cur, pid, name, gender, nationality)
+
+# The find a player from a country with the most gold medals option
+def option_1() :  
+
+    # What is the country
+    try:
+        country = raw_input("What is the player's country")
+    except ValueError:
+        raise CountryError("Invalid country")
+
+    m.player_with_most_gold_medals(cur, country)
+
 connection.close()
 
 
