6a6c58a97184f03d752f109781255b45925fb79c
JiapengWu <jiapeng.wu@mail.mcgill.ca>
2018-03-20 15:26:17 -0400

    added while loop

--- main.py
+++ main.py
@@ -1,52 +1,75 @@
 #!/usr/bin/python
 import modules as m
 import psycopg2
+import logging
 
-config = "user='cs421g19' host='comp421.cs.mcgill.ca' dbname='cs421' password='Pmdd0301'"
-
-connection = psycopg2.connect(config)
-cur = connection.cursor()
-
-option = input("select 1 to execute query 1\nselect 2 to execute query 2\nselect 3 to execute query 3\nselect 4 to execute query 4\nselect 5 to execute query 5")
-
-# Redirects to option that user chose
-if(option == 1) option_1
-elif (option == 2) option_2
-elif (option == 3) option_3
-elif (option == 4) option_4
-elif (option == 5) option_5
-else print 'You did not choose a correct option.'
-
-# The add single player option
-def option_1() :
+def option_1(cur):
     # Input player ID
-    pid = input("What is the player's pid? It has to be greater than 0")
-    if (pid < 0) raise PIDError('Pid less than 0')
+    while True:
+        pid = input("What is the player's pid? It has to be greater than 0")
+        if (pid < 0):
+            print('Pid less than 0')
+            continue
+        # Input name
+        try:
+            name = raw_input("What is the player's name?")
+        except ValueError:
+            print("Invalid name")
+            continue
+        # Gender
+        gender = raw_input("What is the player's gender(Male or Female)")
+        if (gender != "Male" and gender != 'Female'):
+            print('Invalid input')
+            continue
+        # What is the nationality
+        try:
+            nationality = raw_input("What is the player's country")
+        except ValueError:
+            print("Invalid nationality")
+            continue
+        try:
+            m.add_single_player(cur, pid, name, gender, nationality)
+            logging.INFO("Successful.")
+            break
 
-    # Input name
-    try:
-        name = raw_input("What is the player's name?")
-    except ValueError:
-        raise NameError("Invalid name")
-    # Gender
-    gender = raw_input("What is the player's gender") 
-    if (gender != "Male" and gender != 'Female') raise GenderError('Unvalid gender')
+        except Exception as e:
+            if hasattr(e, 'message'):
+                print(e.message)
+            else:
+                print(e)
 
-    # What is the nationality
-    try:
-        nationality = raw_input("What is the player's country")
-    except ValueError:
-        raise NatError("Invalid nationality")
+def main():
+    config = "user='cs421g19' host='comp421.cs.mcgill.ca' dbname='cs421' password='Pmdd0301'"
 
-    m.add_single_player(cur, pid, name, gender, nationality)
+    connection = psycopg2.connect(config)
+    cur = connection.cursor()
 
-# The find a player from a country with the most gold medals option
-def option_1() :  
+    option = input(
+        '''select 1 to execute query 1
+         select 2 to execute query 2
+         select 3 to execute query 3
+         select 4 to execute query 4
+         select 5 to quit
+        ''')
 
-    # What is the country
-    try:
-        country = raw_input("What is the player's country")
-    except ValueError:
-        raise CountryError("Invalid country")
+    # Redirects to option that user chose
+    while True:
+        if(option == 1):
+            option_1(cur)
+        elif (option == 2):
+            pass
+        elif (option == 3):
+            pass
+        elif (option == 4):
+            pass
+        elif (option == 5):
+            break
+        else:
+            logging.WARNING('You did not choose a correct option.')
+    connection.close()
+    # The add single player option
+
+
+main()
 
 
