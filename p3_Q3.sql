d0cbee6f1d76233ecd690c7c1e4cc9a44dab379f
JiapengWu <jiapeng.wu@mail.mcgill.ca>
2018-03-25 17:54:31 -0400

    WIP

--- p3_Q3.sql
+++ p3_Q3.sql
@@ -1,8 +1,30 @@
 CREATE INDEX pcname ON player(cname);
 DROP INDEX pcname;
 
--- easier to run query update_country_medal: Faster to find the players associated 
--- with the country for which we want to update the medal count
+-- easier to run query update_country_medal: 
+
+-- Update country
+--	set gold_number = 
+--	(select sum(player.gold_number) from player
+--	 where country.cname = player.cname
+--	),
+--	silver_number = 
+--	(select sum(player.silver_number) from player
+--	 where country.cname = player.cname
+--	),
+--	bronze_number = 
+--	(select sum(player.bronze_number) from player
+--	 where country.cname = player.cname
+--	),
+--	total_medal_number = 
+--	(select sum(player.gold_number)+sum(player.silver_number)+sum(player.bronze_number) from player
+--	 where country.cname = player.cname
+--	);
+-- In this query we want to update the medal count for countries. 
+-- For each tuple in the country table we look for 
+-- the players with the same country name, thus creating an index on 
+-- cname of player makes it faster to find the associated players
+
 
 CREATE INDEX goldplayer ON player(gold_number);
 DROP INDEX goldplayer;
